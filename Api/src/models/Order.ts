82b15b78e6b6f9f8d22d8f276a6acd974adf281b
renanmdreis <renanmdreis@alu.ufc.br>
2022-11-20 19:18:12 -0300

    Feat - Models/Order

--- Api/src/models/Order.ts
+++ Api/src/models/Order.ts
@@ -14,5 +14,19 @@ export const Order = model('Product', new Schema({
   createdAt: {
     type: Date,
     default: Date.now,
+  },
+  products: {
+    required: true,
+    type: [{
+      product: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Product',
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
+    }]
   }
 }));
