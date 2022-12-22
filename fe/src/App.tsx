1162ee7a45bc7c2f077929ee2c4f807cb875bb69
renanmdreis <renanmdreis@alu.ufc.br>
2022-12-22 10:30:28 -0300

    GlobalStyles

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,3 +1,10 @@
+import { GlobalStyles } from "./styles/GlobalStyles"
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <GlobalStyles />
+      <h1>Hello world</h1>
+    </div>
+  )
 }
