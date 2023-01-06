d81b9abd5c1d92cd25c279934e104281122283bb
renanmdreis <renanmdreis@alu.ufc.br>
2023-01-05 22:33:06 -0300

    Orders - Board + OrdersContainer styles

--- fe/src/components/Orders/styles.ts
+++ fe/src/components/Orders/styles.ts
@@ -10,6 +10,10 @@ export const Board = styled.div`
   padding: 16px;
   border: 1px solid rgba(204, 204, 204, 0.4);
   border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
   header {
     padding: 8px;
@@ -21,6 +25,27 @@ export const Board = styled.div`
 `;
 
 export const OrdersContainer = styled.div`
+  display: flex;
+  flex-direction: column;
 
+  button {
+    background: #fff;
+    border: 1px solid rgba(204, 204, 204, 0.4);
+    height: 128px;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 4px;
+
+    strong {
+      font-weight: 500;
+    }
+    
+    & + button {
+      margin-top: 24px;
+    }
+  }
 `;
 
